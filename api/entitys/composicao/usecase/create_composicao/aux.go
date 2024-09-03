f4168a2c181fb79c18db7445f22d688c3015a36f
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2024-09-03 16:11:17 -0300

    ok

---
+++ api/entitys/composicao/usecase/create_composicao/aux.go
@@ -0,0 +1,13 @@
+package create_composicao
+
+import (
+	mdc "github.com/reizzao/composicao/api/entitys/composicao/modelcomposicao"
+)
+
+func auxCreateRequest(r mdc.ResRequest) mdc.ResRequest {
+	return r
+}
+
+func auxCreateComputed(c mdc.ResComputed) mdc.ResComputed {
+	return c
+}
--- api/entitys/composicao/usecase/create_composicao/create.go
+++ api/entitys/composicao/usecase/create_composicao/create.go
@@ -4,14 +4,6 @@ import (
 	mdc "github.com/reizzao/composicao/api/entitys/composicao/modelcomposicao"
 )
 
-func auxCreateRequest(r mdc.ResRequest) mdc.ResRequest {
-	return r
-}
-
-func auxCreateComputed(c mdc.ResComputed) mdc.ResComputed {
-	return c
-}
-
 func CreateNew(r mdc.ResRequest, c mdc.ResComputed) mdc.ComposicaoModel {
 
 	request := auxCreateRequest(r)
