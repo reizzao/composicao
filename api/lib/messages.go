5b17027b975d176edc9f4b924a02465566a21223
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2024-09-04 16:50:49 -0300

    ok

--- api/lib/messages.go
+++ api/lib/messages.go
@@ -1,5 +1,16 @@
 package lib
 
+// import (
+// 	"testing"
+// )
+
+// var t *testing.T
+
 func MessageErrorTest() string {
 	return "\n ***********RESPONSE TEST *************\n Ops... Esperado: %t --> Tentativa: %t \n*********** FINAL RESPONSE TEST ************* "
 }
+
+// func MessageErrorTest(compare any, expect any)  {
+// 	t.Error("\n ***********RESPONSE TEST *************\n Ops... Esperado: %t --> Tentativa: %t \n*********** FINAL RESPONSE TEST ************* ", expect, compare)
+
+// }
