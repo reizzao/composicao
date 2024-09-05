6c22e5c839010a0a669402e3ebbe4418ec054653
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2024-09-05 09:23:11 -0300

    feat: importando message de test vindo de lib propria via github

--- api/go.mod
+++ api/go.mod
@@ -2,6 +2,4 @@ module github.com/reizzao/composicao/api
 
 go 1.20
 
-require github.com/reizzao/musicalidade/api v0.0.0-20240830143233-2e8c432edcbd
-
-require github.com/reizzao/RzLibs_GO v0.0.0-20240830142918-036dd59c816d // indirect
+require github.com/reizzao/RzLibs_GO v0.0.0-20240905120958-ade110cc50ca
--- api/lib/messages.go
+++ api/lib/messages.go
@@ -1,16 +1,7 @@
 package lib
 
-// import (
-// 	"testing"
-// )
+import (
+	bt "github.com/reizzao/RzLibs_GO/message"
+)
 
-// var t *testing.T
-
-func MessageErrorTest() string {
-	return "\n ***********RESPONSE TEST *************\n Ops... Esperado: %t --> Tentativa: %t \n*********** FINAL RESPONSE TEST ************* "
-}
-
-// func MessageErrorTest(compare any, expect any)  {
-// 	t.Error("\n ***********RESPONSE TEST *************\n Ops... Esperado: %t --> Tentativa: %t \n*********** FINAL RESPONSE TEST ************* ", expect, compare)
-
-// }
+var MessageErrorTest func() string = bt.LibMessageErrorTest
