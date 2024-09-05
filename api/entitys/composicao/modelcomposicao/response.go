c51b3f0cce1950ad3703057b827ed63c9848cf2d
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2024-09-05 15:57:47 -0300

    feat: add entradas padrao voz

--- api/entitys/composicao/modelcomposicao/response.go
+++ api/entitys/composicao/modelcomposicao/response.go
@@ -16,4 +16,8 @@ const (
 	ENTRA_em_1e EntradaPadraoVozOptions = "ENTRA_em_1e"
 	ENTRA_em_2  EntradaPadraoVozOptions = "ENTRA_em_2"
 	ENTRA_em_2e EntradaPadraoVozOptions = "ENTRA_em_2e"
+	ENTRA_em_3  EntradaPadraoVozOptions = "ENTRA_em_3"
+	ENTRA_em_3e EntradaPadraoVozOptions = "ENTRA_em_3e"
+	ENTRA_em_4  EntradaPadraoVozOptions = "ENTRA_em_4"
+	ENTRA_em_4e EntradaPadraoVozOptions = "ENTRA_em_4e"
 )
