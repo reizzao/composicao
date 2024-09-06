152af66d618110c07537f7d261f9d8ded35f1680
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2024-09-06 11:06:23 -0300

    test: computed

--- composicao/usecase/create_composicao/test.create_test.go
+++ composicao/usecase/create_composicao/test.create_test.go
@@ -3,7 +3,7 @@ package create_composicao
 import (
 	"testing"
 
-	itt "github.com/reizzao/music/composicao/literals/inputtester/itt_exemplo_racanegra_tardedemais"
+	inp "github.com/reizzao/music/composicao/literals/inputtester/itt_exemplo_racanegra_tardedemais"
 	mdc "github.com/reizzao/music/composicao/modelcomposicao"
 	lib "github.com/reizzao/music/helpers/lib"
 )
@@ -12,23 +12,31 @@ type ResSut = mdc.ComposicaoModel
 
 var (
 	sut ResSut = CreateNew(
-		itt.InputTestRequest_TardeDemais,
-		itt.InputTestComputed_TardeDemais,
+		inp.InputTestRequest_TardeDemais,
+		inp.InputTestComputed_TardeDemais,
 	)
-	inputTestRequest    mdc.ResRequest              = itt.InputTestRequest_TardeDemais
+	inputTestRequest    mdc.ResRequest              = inp.InputTestRequest_TardeDemais
 	compare_entrada_voz mdc.EntradaPadraoVozOptions = mdc.ENTRA_em_1
-	// inputTestComputed mdc.ResRequest = itt.InputTestComputed_TardeDemais
 )
 
 func Test_Entity(t *testing.T) {
+	// useVars
 	realExpected_entradaVoz := sut.Request.DadosMusica.EntradaPadraoVoz
 	tryTest_entradaVoz := compare_entrada_voz
+
+	realExpected_computed := sut.Computed
+	tryTest_computed := "foo"
 
 	/* -- Suites -- */
 
 	// SUITE ::  Test [EntradaPadraoVozOptions] - TITULO: deve retornar o valor de { EntradaPadraoVozOptions }
 	if realExpected_entradaVoz != tryTest_entradaVoz {
 		t.Error(lib.MessageErrorTest(realExpected_entradaVoz, tryTest_entradaVoz))
+	}
+
+	// SUITE ::  Test [Computed] - TITULO: deve retornar o valor de { Computed }
+	if realExpected_computed != tryTest_computed {
+		t.Error(lib.MessageErrorTest(realExpected_computed, tryTest_computed))
 	}
 
 }
