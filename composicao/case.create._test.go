7f131071124998e6a7844bf711563ae1b3b3b8e6
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2024-09-11 12:06:21 -0300

    ok

--- composicao/case.create._test.go
+++ composicao/case.create._test.go
@@ -19,22 +19,22 @@ var (
 
 func Test_Entity(t *testing.T) {
 	// useVars
-	realExpehelled_entradaVoz := sut.Request.DadosMusica.EntradaPadraoVoz
-	tryTest_entradaVoz := compare_entrada_voz
+	sut_entradaVoz := sut.Request.DadosMusica.EntradaPadraoVoz
+	try_entradaVoz := compare_entrada_voz
 
-	realExpehelled_computed := sut.Computed
-	tryTest_computed := "foo"
+	sut_computed := sut.Computed
+	try_computed := "foo"
 
 	/* -- Suites -- */
 
 	// SUITE ::  Test [EntradaPadraoVozOptions] - TITULO: deve retornar o valor de { EntradaPadraoVozOptions }
-	if realExpehelled_entradaVoz != tryTest_entradaVoz {
-		t.Error(help.MessageErrorTest(realExpehelled_entradaVoz, tryTest_entradaVoz))
+	if sut_entradaVoz != try_entradaVoz {
+		t.Error(help.MessageErrorTest(sut_entradaVoz, try_entradaVoz))
 	}
 
 	// SUITE ::  Test [Computed] - TITULO: deve retornar o valor de { Computed }
-	if realExpehelled_computed != tryTest_computed {
-		t.Error(help.MessageErrorTest(realExpehelled_computed, tryTest_computed))
+	if sut_computed != try_computed {
+		t.Error(help.MessageErrorTest(sut_computed, try_computed))
 	}
 
 }
