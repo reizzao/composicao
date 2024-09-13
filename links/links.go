721be66fccccd5487a0185a4f7cb0ba80aab523c
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2024-09-13 12:27:09 -0300

    feat: links inspiracoes

--- links/links.go
+++ links/links.go
@@ -1,8 +1,11 @@
 package links
 
 var Inspiracoes_Frases = map[string]string{
-	"face_karinearaujo": "https://www.facebook.com/karine.araujo.1829",
-	"todo1":             "todo",
-	"todo2":             "todo",
-	"todo3":             "todo",
+	"face_karinearaujo":            "https://www.facebook.com/karine.araujo.1829",
+	"o_pensador_frases_96":         "https://www.pensador.com/frases_de_musicas_para_compartilhar/",
+	"o_pensador_frases_76":         "https://www.pensador.com/frases_inspiradoras_curtas/",
+	"o_pensador_frases_106_curtas": "https://www.pensador.com/frases_curtas/",
+	"o_pensador_frases_marcantes":  "mensagens_marcantes_frases_curtas_fortes",
+	"todo2":                        "todo",
+	"todo3":                        "todo",
 }
