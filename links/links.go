a1e8b888f2dbbae8ef469751aab89dc0cc21c114
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2024-09-13 12:37:18 -0300

    feat: links inspiracoes

--- links/links.go
+++ links/links.go
@@ -1,11 +1,13 @@
 package links
 
 var Inspiracoes_Frases = map[string]string{
+	"google_frases_curtas":            "https://www.google.com/search?q=ideias+frases+curtas&sca_esv=df37aecebb720a1d&sca_upv=1&sxsrf=ADLYWIIwygXdF81-Bql-PcrGDkAfqVZt0w%3A1726240125141&ei=fVXkZvisCODb5OUP1IaSqQI&ved=0ahUKEwi439LLmcCIAxXgLbkGHVSDJCUQ4dUDCA8&uact=5&oq=ideias+frases+curtas&gs_lp=Egxnd3Mtd2l6LXNlcnAiFGlkZWlhcyBmcmFzZXMgY3VydGFzMgYQABgWGB4yBhAAGBYYHjIIEAAYgAQYogQyCBAAGIAEGKIESM_EA1D-EFiGvgNwAngBkAEAmAHNAaABpBWqAQYwLjE2LjG4AQPIAQD4AQGYAg-gAvERwgIKEAAYsAMY1gQYR8ICBBAhGArCAggQIRigARjDBJgDAOIDBRIBMSBAiAYBkAYIkgcGMi4xMS4yoAfLPQ&sclient=gws-wiz-serp",
 	"face_karinearaujo":            "https://www.facebook.com/karine.araujo.1829",
 	"o_pensador_frases_96":         "https://www.pensador.com/frases_de_musicas_para_compartilhar/",
 	"o_pensador_frases_76":         "https://www.pensador.com/frases_inspiradoras_curtas/",
 	"o_pensador_frases_106_curtas": "https://www.pensador.com/frases_curtas/",
-	"o_pensador_frases_marcantes":  "mensagens_marcantes_frases_curtas_fortes",
+	"o_pensador_frases_marcantes":  "https://www.pensador.com/mensagens_marcantes_frases_curtas_fortes/",
+	"o_pensador_frases_pensativas":  "https://www.pensador.com/frases_pequenas_impactantes/",
 	"todo2":                        "todo",
 	"todo3":                        "todo",
 }
