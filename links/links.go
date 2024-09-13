126628672107179bc0d2317afe4b85d171a47372
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2024-09-13 12:08:19 -0300

    feat: links inspiracoes

---
+++ links/links.go
@@ -0,0 +1,8 @@
+package links
+
+var Inspiracoes_Frases = map[string]string{
+	"face_karinearaujo": "https://www.facebook.com/karine.araujo.1829",
+	"todo1":             "todo",
+	"todo2":             "todo",
+	"todo3":             "todo",
+}
